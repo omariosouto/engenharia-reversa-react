1759507e29a8ea63a6511f00bcb5c86910201410
Mario Souto <soutomarios@gmail.com>
2020-06-30 18:42:38 -0300

    Final version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function convertToHtml(virtualNode) {
+  console.log(virtualNode);
+
+  if(typeof virtualNode === 'string' || typeof virtualNode === 'number') {
+    return document.createTextNode(`${virtualNode}`);
+  }
+
+  const $domElement = document.createElement(virtualNode.tagName);
+
+  virtualNode.props.children.forEach((virtualChild) => {
+    $domElement.appendChild(convertToHtml(virtualChild));
+  })
+
+  return $domElement;
+}
+
+function render(initialVirtualTree, $domRoot) {
+  // console.log(JSON.stringify(initialVirtualTree, null, 4));
+
+  const $appHTML = convertToHtml(initialVirtualTree);
+  console.log('$appHTML',$appHTML);
+  $domRoot.appendChild($appHTML);
+}
+
+function createElement(elementType, props, ...children) {
+  const virtualElementProps = {
+    ...props,
+    children
+  }
+
+  if(typeof elementType === "function") {
+    return elementType(virtualElementProps);
+  }
+
+  return {
+    tagName: elementType,
+    props: virtualElementProps
+  };
+}
+
+const React = {
+  createElement,
+};
+
+// ========================================
+
+function Title() {
+  return React.createElement("h1", null, "Bla");
+}
+
+function App(props) {
+  // return (
+  //   React.createElement(
+  //     "section",
+  //     {
+  //       className: "App"
+  //     }, 
+  //     React.createElement(Title, null), 
+  //     React.createElement("div", null, 
+  //     React.createElement("div", null, "0"), 
+  //     React.createElement("button", null, "Incrementar"), 
+  //     React.createElement("button", null, "Decrementar"))
+  //   )
+  // );
+  return (
+    <section className="App">
+      <h1>Contador</h1>
+      <div>
+        <div>0</div>
+        <button>Incrementar</button>
+        <button>Decrementar</button>
+      </div>
+    </section>
+  );
+}
+
+render(React.createElement(App, null), document.querySelector('#root'));
